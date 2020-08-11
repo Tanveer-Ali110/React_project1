36127b8e808ef40bd3c9a62d3433a15983d39849
Tanveer Ali <ali.tanveer1012@gmail.com>
2020-08-11 13:06:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
 
-    <h1>hello world</h1>
+    <h1>Hello World</h1>
     
   );
 }
