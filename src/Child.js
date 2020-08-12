71f1d1498002fdab15707906e1aa49b90ab0aa96
Tanveer Ali <ali.tanveer1012@gmail.com>
2020-08-12 02:49:31 -0700

    Created Add Transaction

--- src/Child.js
+++ src/Child.js
@@ -6,11 +6,33 @@ function Child() {
     <div className ="container">
         <h1 className="text-center">Expense Tracker</h1>
         <hr/>
-    <div className = "container-element">
 
+    <div className = "container-element">
+        <h3>Your Balance<br/> $250</h3>
+
+        <div className= "element-box">
+            <h3>INCOME <br/> $300</h3>
+            <h3>EXPENSE <br/> $200</h3>
+        </div>
     </div>
     
     <div className = "container-element">
+        <h1>Add/New Transaction</h1>
+        <hr/>
+        <br/>
+        <form className="transection-form">
+            <label>
+                Ener Discription <br/>
+                <input type = "text" required/>
+            </label>
+            <br/>
+            <label>
+                Ener Amount <br/>
+                <input type = "number" required/>
+            </label>
+            <br/>
+            <input type="submit" value="Add Transaction"/>
+        </form>
 
     </div>
 
