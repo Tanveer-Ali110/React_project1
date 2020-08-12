64b35d3293e4203133f1743f0cf9a8f530093a71
Tanveer Ali <ali.tanveer1012@gmail.com>
2020-08-12 03:28:00 -0700

    Update Child.js

--- src/Child.js
+++ src/Child.js
@@ -7,39 +7,39 @@ function Child() {
         <h1 className="text-center">Expense Tracker</h1>
         <hr/>
 
-    <div className = "container-element">
-        <h2>Your Balance<br/> $250</h2>
+        <div className = "container-element">
+            <h2>Your Balance<br/> $250</h2>
 
-        <div className= "element-box">
-            <h3>INCOME <br/> $300</h3>
-            <h3>EXPENSE <br/> $200</h3>
+            <div className= "element-box">
+                <h3>INCOME <br/> $300</h3>
+                <h3>EXPENSE <br/> $200</h3>
+            </div>
         </div>
-    </div>
     
-    <div className = "container-element">
-        <h2>New Transaction</h2>
-        <hr/>
-        <br/>
-        <form className="transection-form">
-            <label>
-                Enter Discription <br/>
-                <input type = "text" required/>
-            </label>
+        <div className = "container-element">
+            <h2>New Transaction</h2>
+            <hr/>
             <br/>
-            <label>
-                Enter Amount <br/>
-                <input type = "number" required/>
-            </label>
-            <br/>
-            <input type="submit" value="Add Transaction"/>
-        </form>
+            <form className="transection-form">
+                <label>
+                    Enter Discription <br/>
+                    <input type = "text" required/>
+                </label>
+                <br/>
+                <label>
+                    Enter Amount <br/>
+                    <input type = "number" required/>
+                </label>
+                <br/>
+                <input type="submit" value="Add Transaction"/>
+            </form>
 
-    </div>
+        </div>
 
-    <div className = "container-element">
-        <h2>Histoy</h2>
+        <div className = "container-element">
+            <h2>Histoy</h2>
 
-    </div>
+        </div>
     
     </div>
   );
